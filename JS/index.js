af89defdfc59b1e15014177039b527056d4ca889
JoeTerol <teroljoseeduardo@gmail.com>
2023-04-28 20:17:42 -0300

    feat: validations ok

--- JS/index.js
+++ JS/index.js
@@ -2,10 +2,11 @@ const items = []
 
 function createItems(item, items){
    items.push(item)
-  return item
+  return console.log(item)
 }
 createItems ({name: "Carlos", id: 33}, items);
 createItems ({name: "Luis", id: 34}, items);
+
 function getAllItems (array){
   return(array)
 }
@@ -13,8 +14,8 @@ getAllItems(items)
 console.log(items)
 
 function searchItems(id, items) {
-  if (!id) {
-    console.log("El ID es requerido");
+  if (isNaN(id)) {
+    console.log("El ID debe ser de tipo number");
   }
 const foundItem = items.find(item => item.id === id);
 
@@ -23,8 +24,8 @@ return foundItem
 searchItems(33, items);
 
 function updateItems(id,data,items){
-  if (isNaN(id)){
-    console.log("La entrada tiene que ser de tipo numero")
+  if (isNaN(id)|| typeof data !== "string"){
+    console.log("La entrada tiene que ser de tipo numero y texto")
   }
 const itemSearch = items.find(i => i.id === id);
       itemSearch.name = data; 
@@ -34,12 +35,16 @@ return itemSearch
 updateItems(33, "Pablo", items)
 
 function deleteProducts (id,items) {
+  if (isNaN(id)) { 
+    console.log("ingrese un numero")
+  }
   const indexItem = items.findIndex(item => item.id === id);
   if (indexItem !== -1){
   items.splice(indexItem, 1);
+  console.log("el item con con el ID", id ,"ha sido eliminado");
   }else{
     console.log("ingrese un numero valido")
-  } 
+  }
   return items;
 }
 deleteProducts(33, items)
