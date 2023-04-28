1ce1f3978cb93e99f2b73ba7af43cb6c60a84c7c
JoeTerol <teroljoseeduardo@gmail.com>
2023-04-28 10:18:19 -0300

    new JS file

---
+++ JS/index.js
@@ -0,0 +1,41 @@
+const items = []
+
+function createItems(item, Items){
+   Items.push(item)
+  return item
+}
+createItems ({name: "Carlos", id: 33}, items);
+createItems ({name: "Luis", id: 34}, items);
+
+function getAllItems (array){
+  return(array)
+}
+getAllItems(items)
+
+function searchItems(id, items) {
+  if (!id) {
+    console.log("El ID es requerido");
+  }
+const foundItem = items.find(item => item.id === id);
+
+return foundItem
+}
+searchItems(33, items);
+
+function updateItems(id,data,items){
+const itemSearch = items.find(i => i.id === id);
+      itemSearch.name = data; 
+return itemSearch
+}
+
+updateItems(33, "Pablo", items)
+
+function deleteProducts (id,items) {
+  const indexItem = items.findIndex(item => item.id === id);
+  if (indexItem !== -1){
+  items.splice(indexItem, 1);
+  } 
+  return items;
+}
+deleteProducts(33, items)
+
